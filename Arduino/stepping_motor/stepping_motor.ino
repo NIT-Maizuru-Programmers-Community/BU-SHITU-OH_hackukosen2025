f36381091065e31d98e8290ae144d9aee21af422
Tikuwa404 <mashidori2@gmail.com>
2025-12-17 18:14:27 +0900

    ✨命令「R;」を追加

--- Arduino/stepping_motor/stepping_motor.ino
+++ Arduino/stepping_motor/stepping_motor.ino
@@ -1,32 +1,22 @@
+constexpr int direction = 1;  //1 or -1
+
 constexpr uint8_t APHASE1 = 2;
-constexpr uint8_t AENBL1 = 3;
-constexpr uint8_t BPHASE1 = 4;
-constexpr uint8_t BENBL1 = 5;
-constexpr uint8_t APHASE2 = 6;
-constexpr uint8_t AENBL2 = 7;
-constexpr uint8_t BPHASE2 = 8;
-constexpr uint8_t BENBL2 = 9;
-constexpr uint8_t APHASE3 = 10;
-constexpr uint8_t AENBL3 = 11;
-constexpr uint8_t BPHASE3 = 12;
-constexpr uint8_t BENBL3 = 13;
+constexpr uint8_t BPHASE1 = 3;
+constexpr uint8_t APHASE2 = 4;
+constexpr uint8_t BPHASE2 = 5;
+constexpr uint8_t APHASE3 = 6;
+constexpr uint8_t BPHASE3 = 7;
+constexpr uint8_t SWITCH1 = 8;
+constexpr uint8_t SWITCH2 = 9;
+constexpr uint8_t SWITCH3 = 10;
 
 int speed[3] = {};  //-90~-12,0,12~90 [°/s]
 int table[9] = {};  //[{mode, pulse_tick, mod_pulse_tick}]
 
-void reset(uint8_t phase1, uint8_t enbl1, uint8_t phase2, uint8_t enbl2) {
-  pinMode(phase1, OUTPUT);
-  pinMode(enbl1, OUTPUT);
-  pinMode(phase2, OUTPUT);
-  pinMode(enbl2, OUTPUT);
-  digitalWrite(enbl1, HIGH);
-  digitalWrite(enbl2, HIGH);
-}
-
 void control(uint8_t phase1, uint8_t phase2, int* tbl) {
   if (++tbl[2] >= tbl[1] * 4) tbl[2] = 0;
 
-  if (tbl[0] > 0) {
+  if (tbl[0] * direction > 0) {
     if (tbl[2] == tbl[1]) {
       digitalWrite(phase1, HIGH);
     } else if (tbl[2] == tbl[1] * 2) {
@@ -39,7 +29,7 @@ void control(uint8_t phase1, uint8_t phase2, int* tbl) {
   } else if (tbl[0] == 0) {
     digitalWrite(phase1, LOW);
     digitalWrite(phase2, LOW);
-  } else if (tbl[0] < 0) {
+  } else if (tbl[0] * direction < 0) {
     if (tbl[2] == tbl[1]) {
       digitalWrite(phase2, HIGH);
     } else if (tbl[2] == tbl[1] * 2) {
@@ -54,40 +44,107 @@ void control(uint8_t phase1, uint8_t phase2, int* tbl) {
 
 void setup() {
   Serial.begin(115200);
-  reset(APHASE1, AENBL1, BPHASE1, BENBL1);
-  reset(APHASE2, AENBL2, BPHASE2, BENBL2);
-  reset(APHASE3, AENBL3, BPHASE3, BENBL3);
+
+  pinMode(APHASE1, OUTPUT);
+  pinMode(BPHASE1, OUTPUT);
+  pinMode(APHASE2, OUTPUT);
+  pinMode(BPHASE2, OUTPUT);
+  pinMode(APHASE3, OUTPUT);
+  pinMode(BPHASE3, OUTPUT);
+  pinMode(SWITCH1, INPUT_PULLUP);
+  pinMode(SWITCH2, INPUT_PULLUP);
+  pinMode(SWITCH3, INPUT_PULLUP);
 }
 
 void loop() {
   if (Serial.available()) {
     String str = Serial.readStringUntil(';');
     char top = str[0];
-    str.remove(0, 1);
-    int spd = str.toInt();
-    if (top == 'A') {
-      table[0] = spd / abs(spd);
-      table[1] = 20000 / (abs(spd) / (5.625 / 64));
+    if (top == 'R') {
+      Serial.println("-90 -90 -90");
+      bool reached[3] = {};
+      table[0] = -1;
+      table[1] = 19;
       table[2] = 0;
-      speed[0] = spd;
-    }
-    if (top == 'B') {
-      table[3] = spd / abs(spd);
-      table[4] = 20000 / (abs(spd) / (5.625 / 64));
+      table[3] = -1;
+      table[4] = 19;
       table[5] = 0;
-      speed[1] = spd;
-    }
-    if (top == 'C') {
-      table[6] = spd / abs(spd);
-      table[7] = 20000 / (abs(spd) / (5.625 / 64));
+      table[6] = -1;
+      table[7] = 19;
       table[8] = 0;
-      speed[2] = spd;
+      while (!(reached[0] && reached[1] && reached[2])) {
+        if (!reached[0] && digitalRead(SWITCH1)) {
+          reached[0] = true;
+          table[0] = 0;
+          table[1] = 0;
+          table[2] = 0;
+        }
+        if (!reached[1] && digitalRead(SWITCH2)) {
+          reached[1] = true;
+          table[3] = 0;
+          table[4] = 0;
+          table[5] = 0;
+        }
+        if (!reached[2] && digitalRead(SWITCH3)) {
+          reached[2] = true;
+          table[6] = 0;
+          table[7] = 0;
+          table[8] = 0;
+        }
+        control(APHASE1, BPHASE1, table);
+        control(APHASE2, BPHASE2, table + 3);
+        control(APHASE3, BPHASE3, table + 6);
+        delayMicroseconds(100);
+      }
+      speed[0] = 0;
+      speed[1] = 0;
+      speed[2] = 0;
+      Serial.println("0 0 0");
+    } else {
+      str.remove(0, 1);
+      int spd = str.toInt();
+      if (top == 'A') {
+        if (spd == 0) {
+          table[0] = 0;
+          table[1] = 0;
+          table[2] = 0;
+        } else {
+          table[0] = spd / abs(spd);
+          table[1] = 20000 / (abs(spd) / (5.625 / 64));
+          table[2] = 0;
+        }
+        speed[0] = spd;
+      }
+      if (top == 'B') {
+        if (spd == 0) {
+          table[3] = 0;
+          table[4] = 0;
+          table[5] = 0;
+        } else {
+          table[3] = spd / abs(spd);
+          table[4] = 20000 / (abs(spd) / (5.625 / 64));
+          table[5] = 0;
+        }
+        speed[1] = spd;
+      }
+      if (top == 'C') {
+        if (spd == 0) {
+          table[6] = 0;
+          table[7] = 0;
+          table[8] = 0;
+        } else {
+          table[6] = spd / abs(spd);
+          table[7] = 20000 / (abs(spd) / (5.625 / 64));
+          table[8] = 0;
+        }
+        speed[2] = spd;
+      }
+      Serial.print(speed[0]);
+      Serial.print(' ');
+      Serial.print(speed[1]);
+      Serial.print(' ');
+      Serial.println(speed[2]);
     }
-    Serial.print(speed[0]);
-    Serial.print(' ');
-    Serial.print(speed[1]);
-    Serial.print(' ');
-    Serial.println(speed[2]);
   }
 
   control(APHASE1, BPHASE1, table);
